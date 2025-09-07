e2f4e3706575e1316739fd0238bd826f81039f42
Sahar Ghadampoor Hafezi <sahar.gh.hafezi.dev@gmail.com>
2025-09-08 01:34:35 +0330

    [feature] - implement base of client

--- server/app.js
+++ server/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const cors = require('cors');
 const { uploadFile, deleteFile, listFile } = require("./middleware/multer");
 const app = express();
 const port = process.env.PORT || 3500;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const corsOptions = {
+   origin: "http://localhost:3000",//(https://your-client-app.com)
+   optionsSuccessStatus: 200,
+ };
+
+// Use CORS middleware
+ app.use(cors(corsOptions));
 
 app.post("/upload", uploadFile.single("image"), (req, res) => {
   console.log(req.body);
